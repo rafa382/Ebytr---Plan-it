27a6d489496ebcda7d9232a3d4dc184eccd82437
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2022-07-04 19:39:40 -0300

    Feat: Página principal responsável pela renderização do componente Task Manager.

---
+++ ebytr-project-plan-it/src/pages/mainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TaskManager from "../components/taskManager";
+
+class MainPage extends React.Component {
+render() {
+  return(
+    <div>
+      <TaskManager />  
+    </div>
+  );
+}
+}
+
+export default MainPage;
