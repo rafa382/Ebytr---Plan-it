ada1be162df5dae9c18f07b25011f41d7c400516
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2022-07-04 19:46:13 -0300

    Feat: Renderização do titulo e da página principal.

---
+++ ebytr-project-plan-it/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import './style/headerStyle.css';
+import './style/inputTaskStyle.css';
+import './style/taskList.css';
+import './style/addButtonStyle.css';
+import MainPage from './pages/mainPage';
+import React, { useState } from 'react';
+import TaskManager from './components/taskManager';
+
+function App() {
+
+  return (
+    <div>
+      <div className='header'>
+      <h1 className='title'>Ebtr Plan It</h1>
+      </div>
+        <MainPage />
+    </div>
+  );
+}
+
+export default App;
+
